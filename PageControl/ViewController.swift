cbb50b4082bc4159b967b16b9fb0c58d2f9ae9fd
kingofahn <kingofahn@gmail.com>
2019-06-14 13:30:55 +0900

    Initial Commit

---
+++ PageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by kingofahn on 14/06/2019.
+//  Copyright © 2019 kingofahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
